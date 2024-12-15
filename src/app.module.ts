6990dae4cfa373ae2269299b110ba73684749cfd
spider <spider@spider.local>
2024-12-15 20:42:21 +0100

    generated endpoint setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FilmsModule } from './films/films.module';
+import { SpeciesModule } from './species/species.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { StarshipsModule } from './starships/starships.module';
+import { PlanetsModule } from './planets/planets.module';
 
 @Module({
   imports: [
@@ -11,6 +16,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    FilmsModule,
+    SpeciesModule,
+    VehiclesModule,
+    StarshipsModule,
+    PlanetsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
